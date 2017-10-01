c5019515ede88b6734504d68da56ad605c3e7f72
zolangi <zolangiramirez@gmail.com>
2017-09-30 20:50:34 -0700

    Update file

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable }    from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
@@ -8,43 +8,42 @@ import { User } from './user';
 @Injectable()
 export class UserService {
   private headers = new Headers({'Content-Type' : 'application/json'});
-  private heroesUrl = 'www.shayng.com/api/heroes';  // URL to web api
 
   constructor(private http: Http) { }
 
-  getHero(id: number): Promise<Hero> {
-    const url = `${this.heroesUrl}/${id}`;
+  getUser(id: number): Promise<User> {
+    const url = `${this.usersUrl}/${id}`;
     return this.http.get(url)
       .toPromise()
-      .then(response => response.json().data as Hero)
+      .then(response => response.json().data as User)
       .catch(this.handleError);
   }
 
-  getHeroes(): Promise<Hero[]> {
-    return this.http.get(this.heroesUrl)
+  getUsers(): Promise<User[]> {
+    return this.http.get(this.usersUrl)
       .toPromise()
-      .then(response => response.json().data as Hero[])
+      .then(response => response.json().data as User[])
       .catch(this.handleError);
   }
 
-  update(hero: Hero): Promise<Hero> {
-    const url = `${this.heroesUrl}/${hero.id}`;
+  update(user: User): Promise<User> {
+    const url = `${this.heroesUrl}/${user.id}`;
     return this.http
-      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .put(url, JSON.stringify(user), {headers: this.headers})
       .toPromise()
-      .then(() => hero)
+      .then(() => user)
       .catch(this.handleError);
   }
-  create(name: string): Promise<Hero> {
+  create(username: string): Promise<User> {
     return this.http
-      .post(this.heroesUrl, JSON.stringify({name: name}), {headers:this.headers})
+      .post(this.heroesUrl, JSON.stringify({username: username}), {headers:this.headers})
       .toPromise()
-      .then(res=> res.json().data as Hero)
+      .then(res=> res.json().data as User)
       .catch(this.handleError)
   }
 
   delete(id: number): Promise<void> {
-    const url = `${this.heroesUrl}/${id}`;
+    const url = `${this.usersUrl}/${id}`;
     return this.http.delete(url, {headers: this.headers})
       .toPromise()
       .then(() => null)
