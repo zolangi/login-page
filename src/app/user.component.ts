e9cfb4c9c29e620b7d27de44b416641151958c60
zolangi <zolangiramirez@gmail.com>
2017-09-30 20:43:30 -0700

    ok

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,15 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+import { User } from './user';
+import { UserService } from './user.service';
+
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-users',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 
-export class UserComponent {
+export class UserComponent implements OnInit{
+  users: User[];
+  selectedUser: User;
+
   constructor(
-     userid: number,
-     username: string,
-     password: string,
-  ){}
+    private router: Router,
+    private userService: UserService) { }
+)
+
+  getUsers(): void {
+    this.userService.getUsers().then(users => this.users = users);
+  }
+
+  add(username: string): void  {
+    username = username.trim();
+    if(!name) { return; }
+    this.userService.create(username)
+      .then(user => {
+        this.users.push(user);
+        this.selectedUser = null;
+      });
+  }
+
+  delete(user: User): void {
+    this.userService
+      .delete(user.id)
+      .then(() => {
+        this.users = this.users.filter(h => h !== user);
+        if (this.selectedUser === user) { this.selectedUser = null; }
+      });
+  }
+
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  onSelect(user: User): void {
+    this.selectedUser = user;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedUser.id]);
+  }
+
+
 }
